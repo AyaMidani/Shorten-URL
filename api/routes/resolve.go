a53b521aece3738845044ad0085df310dc71c53d
Aya Mydani <ayamidani98@gmail.com>
2025-10-06 15:50:29 +0300

    resolve function

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,7 +1,25 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/AyaMidani/Shorten-URL/api/database"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
 
 func ResolveURL(c *fiber.Ctx) error {
-	return c.SendStatus(1)
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short not found in the database"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "cannot connect to db"})
+	}
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
 }
